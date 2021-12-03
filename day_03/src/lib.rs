d31146d634edaaba8e12faa78ef282f47bd786fb
Jan Lek <j.lek@me.com>
2021-12-03 09:03:59 +0000

    Day 3 - First tidy up
    The first of many, I'm sure.

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -5,64 +5,28 @@ use std::cmp::Ordering::{Equal, Greater, Less};
 
 fn part_1(input: &str) -> usize {
     let num_columns = input.lines().next().unwrap().chars().count();
-    let mut counts: Vec<[usize; 2]> = Vec::with_capacity(num_columns);
-    counts.resize(num_columns, [0, 0]);
+    let numbers: Vec<_> = input.lines().collect();
+    let gamma_rate: usize = usize::from_str_radix(
+        &(0..num_columns)
+            .map(|column| char::from(most_common_bit(&numbers, column).unwrap()))
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
 
-    for line in input.lines() {
-        for (i, bit) in line.bytes().map(|b| usize::from(b - b'0')).enumerate() {
-            counts[i][bit] += 1;
-        }
-    }
+    let epsilon_rate: usize = usize::from_str_radix(
+        &(0..num_columns)
+            .map(|column| char::from(least_common_bit(&numbers, column).unwrap()))
+            .collect::<String>(),
+        2,
+    )
+    .unwrap();
 
-    let mut gamma_rate = String::new();
-    let mut epsilon_rate = String::new();
-
-    for count in counts {
-        if count[0] > count[1] {
-            gamma_rate.push('0');
-            epsilon_rate.push('1');
-        } else {
-            gamma_rate.push('1');
-            epsilon_rate.push('0');
-        }
-    }
-
-    usize::from_str_radix(&gamma_rate, 2).unwrap()
-        * usize::from_str_radix(&epsilon_rate, 2).unwrap()
+    gamma_rate * epsilon_rate
 }
 
 fn part_2(input: &str) -> usize {
     let num_columns = input.lines().next().unwrap().chars().count();
-
-    let mut most_common_bits: Vec<Option<u8>> = Vec::with_capacity(num_columns);
-    let mut least_common_bits: Vec<Option<u8>> = Vec::with_capacity(num_columns);
-    for column in 0..num_columns {
-        let mut num_zeros = 0;
-        let mut num_ones = 0;
-        for line in input.lines() {
-            match line.as_bytes()[column] {
-                b'0' => {
-                    num_zeros += 1;
-                }
-                b'1' => {
-                    num_ones += 1;
-                }
-                _ => panic!(),
-            }
-        }
-
-        most_common_bits.push(match num_zeros.cmp(&num_ones) {
-            Less => Some(b'1'),
-            Equal => None,
-            Greater => Some(b'0'),
-        });
-
-        least_common_bits.push(match num_zeros.cmp(&num_ones) {
-            Less => Some(b'0'),
-            Equal => None,
-            Greater => Some(b'1'),
-        });
-    }
 
     let mut oxygen_generator_rating_candidates: Vec<_> = input.lines().collect();
     for column in 0..num_columns {
@@ -78,12 +42,8 @@ fn part_2(input: &str) -> usize {
 
     let mut co2_scrubber_rating_candidates: Vec<_> = input.lines().collect();
     for column in 0..num_columns {
-        let least_common_bit = match most_common_bit(&co2_scrubber_rating_candidates, column) {
-            Some(b'0') => b'1',
-            Some(b'1') => b'0',
-            None => b'0',
-            Some(_) => panic!(),
-        };
+        let least_common_bit =
+            least_common_bit(&co2_scrubber_rating_candidates, column).unwrap_or(b'0');
 
         co2_scrubber_rating_candidates.retain(|line| line.as_bytes()[column] == least_common_bit);
         if co2_scrubber_rating_candidates.len() == 1 {
@@ -116,6 +76,15 @@ fn most_common_bit(numbers: &[&str], column: usize) -> Option<u8> {
         Greater => Some(b'0'),
         Equal => None,
         Less => Some(b'1'),
+    }
+}
+
+fn least_common_bit(numbers: &[&str], column: usize) -> Option<u8> {
+    match most_common_bit(numbers, column) {
+        Some(b'0') => Some(b'1'),
+        Some(b'1') => Some(b'0'),
+        None => None,
+        Some(_) => panic!(),
     }
 }
 
