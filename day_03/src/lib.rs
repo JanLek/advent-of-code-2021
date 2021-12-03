bb82f7742a8f9e3f822568571c880c931e1fbfb3
Jan Lek <j.lek@me.com>
2021-12-03 09:54:02 +0000

    Day 3 - Fourth tidy-up

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,8 +1,6 @@
 #![allow(dead_code)]
 #![deny(clippy::all, clippy::pedantic)]
 
-use std::cmp::Ordering::{Equal, Greater, Less};
-
 fn part_1(input: &str) -> usize {
     let num_columns = input.lines().next().unwrap().chars().count();
     let numbers: Vec<_> = input.lines().collect();
@@ -14,25 +12,16 @@ fn part_1(input: &str) -> usize {
 fn part_2(input: &str) -> usize {
     let num_columns = input.lines().next().unwrap().chars().count();
     let numbers = input.lines().collect::<Vec<_>>();
-    let oxygen_generator_rating =
-        find_rating(num_columns, &numbers, |candidate_numbers, column| {
-            most_common_bit(candidate_numbers, column).unwrap_or(b'1')
-        });
-    let co2_scrubber_rating = find_rating(num_columns, &numbers, |candidate_numbers, column| {
-        least_common_bit(candidate_numbers, column).unwrap_or(b'0')
-    });
+    let oxygen_generator_rating = find_rating(num_columns, &numbers, most_common_bit);
+    let co2_scrubber_rating = find_rating(num_columns, &numbers, least_common_bit);
 
     oxygen_generator_rating * co2_scrubber_rating
 }
 
-fn find_rate(
-    num_columns: usize,
-    numbers: &[&str],
-    choose_bit: fn(&[&str], usize) -> Option<u8>,
-) -> usize {
+fn find_rate(num_columns: usize, numbers: &[&str], choose_bit: fn(&[&str], usize) -> u8) -> usize {
     usize::from_str_radix(
         &(0..num_columns)
-            .map(|column| char::from(choose_bit(numbers, column).unwrap()))
+            .map(|column| char::from(choose_bit(numbers, column)))
             .collect::<String>(),
         2,
     )
@@ -42,11 +31,11 @@ fn find_rate(
 fn find_rating(
     num_columns: usize,
     numbers: &[&str],
-    make_bit_criteria: impl Fn(&[&str], usize) -> u8,
+    bit_criteria: impl Fn(&[&str], usize) -> u8,
 ) -> usize {
     let mut candidate_numbers = Vec::from(numbers);
     for column in 0..num_columns {
-        let bit_criteria = make_bit_criteria(&candidate_numbers, column);
+        let bit_criteria = bit_criteria(&candidate_numbers, column);
         candidate_numbers.retain(|line| line.as_bytes()[column] == bit_criteria);
         if candidate_numbers.len() == 1 {
             break;
@@ -56,7 +45,7 @@ fn find_rating(
     usize::from_str_radix(candidate_numbers[0], 2).unwrap()
 }
 
-fn most_common_bit(numbers: &[&str], column: usize) -> Option<u8> {
+fn most_common_bit(numbers: &[&str], column: usize) -> u8 {
     let mut num_zeros = 0;
     let mut num_ones = 0;
 
@@ -68,19 +57,18 @@ fn most_common_bit(numbers: &[&str], column: usize) -> Option<u8> {
         }
     }
 
-    match num_zeros.cmp(&num_ones) {
-        Greater => Some(b'0'),
-        Equal => None,
-        Less => Some(b'1'),
+    if num_zeros > num_ones {
+        b'0'
+    } else {
+        b'1'
     }
 }
 
-fn least_common_bit(numbers: &[&str], column: usize) -> Option<u8> {
+fn least_common_bit(numbers: &[&str], column: usize) -> u8 {
     match most_common_bit(numbers, column) {
-        Some(b'0') => Some(b'1'),
-        Some(b'1') => Some(b'0'),
-        None => None,
-        Some(_) => panic!(),
+        b'0' => b'1',
+        b'1' => b'0',
+        _ => panic!(),
     }
 }
 
