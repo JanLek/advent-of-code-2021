137e4812f07e12eda5154c99e9f928645f294474
Jan Lek <j.lek@me.com>
2021-12-07 07:34:45 +0000

    Day 3 - Part 1 - Initial implementation

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -1,17 +1,37 @@
 #![allow(dead_code)]
 #![deny(clippy::all, clippy::pedantic)]
-#![feature(test)]
+#![feature(int_abs_diff, test)]
 
-fn part_1(_input: &str) -> usize {
-    todo!()
+fn part_1(input: &str) -> usize {
+    let counts = parse_input(input);
+    (0..=2000)
+        .map(|target_position| calculate_fuel_cost(&counts, target_position))
+        .min()
+        .unwrap()
 }
 
 fn part_2(_input: &str) -> usize {
     todo!()
 }
 
-fn parse_input(_input: &str) {
-    todo!()
+fn parse_input(input: &str) -> [u8; 2000] {
+    let mut positions = [0; 2000];
+
+    for position in input.split(',') {
+        let position: usize = position.parse().unwrap();
+        positions[position] += 1;
+    }
+
+    positions
+}
+
+fn calculate_fuel_cost(positions: &[u8; 2000], target_position: usize) -> usize {
+    positions
+        .iter()
+        .enumerate()
+        .fold(0, |cost, (position, &num_crab_submarines)| {
+            cost + (num_crab_submarines as usize) * position.abs_diff(target_position)
+        })
 }
 
 #[cfg(test)]
@@ -26,7 +46,7 @@ mod tests {
 
     #[test]
     fn test() {
-        assert_eq!(part_1(SAMPLE_INPUT), 0);
+        assert_eq!(part_1(SAMPLE_INPUT), 37);
         assert_eq!(part_1(INPUT), 0);
 
         assert_eq!(part_2(SAMPLE_INPUT), 0);
@@ -34,11 +54,13 @@ mod tests {
     }
 
     #[bench]
+    #[ignore]
     fn bench_part_1(b: &mut Bencher) {
         b.iter(|| part_1(INPUT));
     }
 
     #[bench]
+    #[ignore]
     fn bench_part_2(b: &mut Bencher) {
         b.iter(|| part_2(INPUT));
     }
