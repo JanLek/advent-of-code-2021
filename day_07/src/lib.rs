3c3f63825213edb8ebcf3207d28612cbafffb25a
John <jlek@findmypast.com>
2021-12-07 12:02:21 +0000

    Day 7 - DRY up the code a bit

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -2,43 +2,48 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![feature(int_abs_diff, test)]
 
+use std::convert::identity;
+
 fn part_1(input: &str) -> usize {
-    let counts = parse_input(input);
-    (0..=2000)
-        .map(|target_position| calculate_fuel_cost_incorrectly(&counts, target_position))
-        .min()
-        .unwrap()
+    find_minimal_fuel_cost(&parse_input(input), identity)
 }
 
 fn part_2(input: &str) -> usize {
-    let counts = parse_input(input);
+    find_minimal_fuel_cost(
+        &parse_input(input),
+        |n| n * (n + 1) / 2, // https://en.wikipedia.org/wiki/Triangular_number
+    )
+}
+
+fn find_minimal_fuel_cost(
+    crab_submarine_positions: &[u8; 2000],
+    calculate_fuel_cost_for_distance: fn(usize) -> usize,
+) -> usize {
     (0..=2000)
-        .map(|target_position| calculate_fuel_cost(&counts, target_position))
+        .map(|target_position| {
+            calculate_total_fuel_cost(
+                crab_submarine_positions,
+                target_position,
+                calculate_fuel_cost_for_distance,
+            )
+        })
         .min()
         .unwrap()
 }
 
 fn parse_input(input: &str) -> [u8; 2000] {
     let mut positions = [0; 2000];
-
     for position in input.split(',') {
-        let position: usize = position.parse().unwrap();
-        positions[position] += 1;
+        positions[position.parse::<usize>().unwrap()] += 1;
     }
-
     positions
 }
 
-fn calculate_fuel_cost_incorrectly(positions: &[u8; 2000], target_position: usize) -> usize {
-    positions
-        .iter()
-        .enumerate()
-        .fold(0, |cost, (position, &num_crab_submarines)| {
-            cost + (num_crab_submarines as usize) * position.abs_diff(target_position)
-        })
-}
-
-fn calculate_fuel_cost(positions: &[u8; 2000], target_position: usize) -> usize {
+fn calculate_total_fuel_cost(
+    positions: &[u8; 2000],
+    target_position: usize,
+    fuel_cost_for_distance: fn(usize) -> usize,
+) -> usize {
     positions
         .iter()
         .enumerate()
@@ -46,11 +51,6 @@ fn calculate_fuel_cost(positions: &[u8; 2000], target_position: usize) -> usize 
             cost + (num_crab_submarines as usize)
                 * fuel_cost_for_distance(position.abs_diff(target_position))
         })
-}
-
-fn fuel_cost_for_distance(n: usize) -> usize {
-    // https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
-    n * (n + 1) / 2
 }
 
 #[cfg(test)]
