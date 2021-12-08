a87dd19fd96943593945d59886c41a51d392a1f2
Jan Lek <j.lek@me.com>
2021-12-08 19:17:10 +0000

    Day 8 - Enabled benchmark tests
    Not that it's very fast or anything.

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -146,13 +146,11 @@ mod tests {
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_1(b: &mut Bencher) {
         b.iter(|| part_1(INPUT));
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_2(b: &mut Bencher) {
         b.iter(|| part_2(INPUT));
     }
