6eb748ebf8e1e0b884aadea2163f65df7c08b847
Jan Lek <j.lek@me.com>
2021-12-10 07:35:32 +0000

    Day 10 - Initial implementation

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -2,44 +2,78 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![feature(test)]
 
+const CHUNK_CHARACTERS: [(u8, u8); 4] = [(b'(', b')'), (b'[', b']'), (b'{', b'}'), (b'<', b'>')];
+
 fn part_1(input: &str) -> usize {
     input
         .lines()
-        .filter_map(first_illegal_character)
-        .map(points)
+        .filter_map(|line| match first_illegal_character(line) {
+            ParseResult::InvalidCharacter(c) => Some(c),
+            ParseResult::Incomplete(_) => None,
+        })
+        .map(syntax_error_points)
         .sum()
 }
 
-fn part_2(_input: &str) -> usize {
-    todo!()
+fn part_2(input: &str) -> usize {
+    let mut scores: Vec<_> = input
+        .lines()
+        .filter_map(|line| match first_illegal_character(line) {
+            ParseResult::InvalidCharacter(_) => None,
+            ParseResult::Incomplete(completion_characters) => Some(
+                completion_characters
+                    .into_iter()
+                    .rev()
+                    .fold(0, |score, character| {
+                        score * 5 + autocomplete_points(character)
+                    }),
+            ),
+        })
+        .collect();
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
-const CHUNK_CHARACTERS: [(u8, u8); 4] = [(b'(', b')'), (b'[', b']'), (b'{', b'}'), (b'<', b'>')];
-
-fn first_illegal_character(line: &str) -> Option<u8> {
+fn first_illegal_character(line: &str) -> ParseResult {
     let mut closing_characters = Vec::with_capacity(line.len());
-    let mut seen = String::new();
     for character in line.bytes() {
-        seen.push(char::from(character));
-        if let Some((_, closing_character)) =
+        if let Some(&(_, closing_character)) =
             CHUNK_CHARACTERS.iter().find(|&&(o, _)| o == character)
         {
             closing_characters.push(closing_character);
-        } else if let Some(&expected) = closing_characters.pop() {
+        } else if let Some(expected) = closing_characters.pop() {
             if expected != character {
-                return Some(character);
+                return ParseResult::InvalidCharacter(character);
             }
+        } else {
+            panic!("This line should never be reached");
         }
     }
-    None
+
+    ParseResult::Incomplete(closing_characters)
 }
 
-fn points(byte: u8) -> usize {
+enum ParseResult {
+    InvalidCharacter(u8),
+    Incomplete(Vec<u8>),
+}
+
+fn syntax_error_points(byte: u8) -> usize {
     match byte {
         b')' => 3,
         b']' => 57,
         b'}' => 1197,
         b'>' => 25137,
+        _ => panic!(),
+    }
+}
+
+fn autocomplete_points(byte: u8) -> usize {
+    match byte {
+        b')' => 1,
+        b']' => 2,
+        b'}' => 3,
+        b'>' => 4,
         _ => panic!(),
     }
 }
@@ -61,10 +95,9 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
-        assert_eq!(part_2(SAMPLE_INPUT), 0);
-        // assert_eq!(part_2(INPUT), 0);
+        assert_eq!(part_2(SAMPLE_INPUT), 288_957);
+        assert_eq!(part_2(INPUT), 3_094_671_161);
     }
 
     #[bench]
@@ -73,7 +106,6 @@ mod tests {
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_2(b: &mut Bencher) {
         b.iter(|| part_2(INPUT));
     }
