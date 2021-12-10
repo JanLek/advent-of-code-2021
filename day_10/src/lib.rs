7b95f3edf7876fdc1256dd7084d6b37e7e916580
Jan Lek <j.lek@me.com>
2021-12-10 07:14:29 +0000

    Day 10 - Part 1 - Initial implementation

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -2,12 +2,46 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![feature(test)]
 
-fn part_1(_input: &str) -> usize {
-    todo!()
+fn part_1(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(first_illegal_character)
+        .map(points)
+        .sum()
 }
 
 fn part_2(_input: &str) -> usize {
     todo!()
+}
+
+const CHUNK_CHARACTERS: [(u8, u8); 4] = [(b'(', b')'), (b'[', b']'), (b'{', b'}'), (b'<', b'>')];
+
+fn first_illegal_character(line: &str) -> Option<u8> {
+    let mut closing_characters = Vec::with_capacity(line.len());
+    let mut seen = String::new();
+    for character in line.bytes() {
+        seen.push(char::from(character));
+        if let Some((_, closing_character)) =
+            CHUNK_CHARACTERS.iter().find(|&&(o, _)| o == character)
+        {
+            closing_characters.push(closing_character);
+        } else if let Some(&expected) = closing_characters.pop() {
+            if expected != character {
+                return Some(character);
+            }
+        }
+    }
+    None
+}
+
+fn points(byte: u8) -> usize {
+    match byte {
+        b')' => 3,
+        b']' => 57,
+        b'}' => 1197,
+        b'>' => 25137,
+        _ => panic!(),
+    }
 }
 
 #[cfg(test)]
@@ -22,8 +56,8 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(SAMPLE_INPUT), 26397);
-        // assert_eq!(part_1(INPUT), 0);
+        assert_eq!(part_1(SAMPLE_INPUT), 26_397);
+        assert_eq!(part_1(INPUT), 462_693);
     }
 
     #[test]
@@ -34,7 +68,6 @@ mod tests {
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_1(b: &mut Bencher) {
         b.iter(|| part_1(INPUT));
     }
