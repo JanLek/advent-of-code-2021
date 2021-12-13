1159e0655b4550b290fd2f1e72acdeaa56a85be2
Jan Lek <j.lek@me.com>
2021-12-13 17:09:41 +0000

    Day 13 - Part 2

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -3,9 +3,10 @@
 #![feature(test)]
 
 use std::{
+    collections::HashSet,
     fmt::{Display, Formatter, Result as FormatResult},
     num::ParseIntError,
-    ops::{Index, IndexMut},
+    ops::Index,
     str::FromStr,
 };
 
@@ -16,8 +17,14 @@ fn part_1(input: &str) -> Result<usize, ParseError> {
     Ok(grid.num_points())
 }
 
-fn part_2(_input: &str) -> usize {
-    todo!();
+fn part_2(input: &str) -> Result<usize, ParseError> {
+    let (coordinates, fold_alongs) = parse_input(input)?;
+    let mut grid: Grid = coordinates.into();
+    for fold_along in fold_alongs {
+        grid.fold(fold_along);
+    }
+    println!("{}", grid);
+    Ok(grid.num_points())
 }
 
 fn parse_input(input: &str) -> Result<(Vec<Coordinate>, Vec<FoldAlong>), ParseError> {
@@ -34,7 +41,7 @@ fn parse_input(input: &str) -> Result<(Vec<Coordinate>, Vec<FoldAlong>), ParseEr
     ))
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Coordinate {
     x: usize,
     y: usize,
@@ -83,79 +90,53 @@ impl From<ParseIntError> for ParseError {
 struct Grid {
     num_columns: usize,
     num_rows: usize,
-    points: Vec<bool>,
+    points: Vec<Coordinate>,
 }
 
 impl Grid {
     fn fold(&mut self, fold_along: FoldAlong) {
         match fold_along {
             FoldAlong::X(x_fold) => {
-                let new_num_columns = x_fold;
-                let mut new_points = vec![false; new_num_columns * self.num_rows];
-
-                for y in 0..self.num_rows {
-                    for x in 0..x_fold {
-                        if self[Coordinate { x, y }] {
-                            new_points[y * new_num_columns + x] = true;
-                        }
-                    }
-
-                    for x in x_fold + 1..self.num_columns {
-                        if self[Coordinate { x, y }] {
-                            let new_x = x_fold - (x - x_fold);
-                            new_points[y * new_num_columns + new_x] = true;
-                        }
+                for point in &mut self.points {
+                    if point.x > x_fold {
+                        point.x = x_fold - (point.x - x_fold);
                     }
                 }
-                self.num_columns = new_num_columns;
-                self.points = new_points;
+                self.num_columns = x_fold;
             }
             FoldAlong::Y(y_fold) => {
-                for y in y_fold + 1..self.num_rows {
-                    for x in 0..self.num_columns {
-                        if self[Coordinate { x, y }] {
-                            self[Coordinate {
-                                x,
-                                y: y_fold - (y - y_fold),
-                            }] = true;
-                        }
+                for point in &mut self.points {
+                    if point.y > y_fold {
+                        point.y = y_fold - (point.y - y_fold);
                     }
+                    self.num_rows = y_fold;
                 }
-                self.num_rows = y_fold;
-                self.points.resize(self.num_columns * self.num_rows, false);
             }
         }
     }
 
     fn num_points(&self) -> usize {
-        self.points.iter().filter(|&&point| point).count()
+        self.points.iter().copied().collect::<HashSet<_>>().len()
     }
 }
 
 impl From<Vec<Coordinate>> for Grid {
-    fn from(coordinates: Vec<Coordinate>) -> Self {
-        let num_columns = coordinates
-            .iter()
-            .map(|coordinate| coordinate.x)
-            .max()
-            .unwrap_or_default()
-            + 1;
-        let num_rows = coordinates
-            .iter()
-            .map(|coordinate| coordinate.y)
-            .max()
-            .unwrap_or_default()
-            + 1;
-
-        let mut grid = Self {
-            num_columns,
-            num_rows,
-            points: vec![false; num_columns * num_rows],
-        };
-        for coordinate in coordinates {
-            grid[coordinate] = true;
+    fn from(points: Vec<Coordinate>) -> Self {
+        Self {
+            num_columns: points
+                .iter()
+                .map(|coordinate| coordinate.x)
+                .max()
+                .unwrap_or_default()
+                + 1,
+            num_rows: points
+                .iter()
+                .map(|coordinate| coordinate.y)
+                .max()
+                .unwrap_or_default()
+                + 1,
+            points,
         }
-        grid
     }
 }
 
@@ -163,13 +144,11 @@ impl Index<Coordinate> for Grid {
     type Output = bool;
 
     fn index(&self, coordinate: Coordinate) -> &Self::Output {
-        &self.points[coordinate.y * self.num_columns + coordinate.x]
-    }
-}
-
-impl IndexMut<Coordinate> for Grid {
-    fn index_mut(&mut self, coordinate: Coordinate) -> &mut Self::Output {
-        &mut self.points[coordinate.y * self.num_columns + coordinate.x]
+        if self.points.contains(&coordinate) {
+            &true
+        } else {
+            &false
+        }
     }
 }
 
@@ -207,21 +186,18 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
-        assert_eq!(part_2(SAMPLE_INPUT), 0);
-        assert_eq!(part_2(INPUT), 0);
+        assert_eq!(part_2(SAMPLE_INPUT).unwrap(), 16);
+        assert_eq!(part_2(INPUT).unwrap(), 98); // Code: FGKCKBZG
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_1(b: &mut Bencher) {
-        b.iter(|| part_1(INPUT));
+        b.iter(|| part_1(INPUT).unwrap());
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_2(b: &mut Bencher) {
-        b.iter(|| part_2(INPUT));
+        b.iter(|| part_2(INPUT).unwrap());
     }
 }
