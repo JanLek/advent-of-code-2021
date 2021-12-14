2b71865a0967dc1014d92246e0fd5789d3c75bb9
Jan Lek <j.lek@me.com>
2021-12-14 17:11:54 +0000

    Day 14 - Rewrite for Part 2
    Wasn't fast enough before.

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -2,27 +2,16 @@
 #![deny(clippy::all, clippy::pedantic)]
 #![feature(test)]
 
-use std::{
-    collections::HashMap,
-    fmt::{Display, Formatter, Result as FormatResult},
-    str::FromStr,
-};
+use std::{collections::HashMap, iter::IntoIterator, str::FromStr};
 
 fn part_1(input: &str) -> Result<usize, ParseError> {
-    // let (polymer, pair_insertion_rules) = parse_input(input)?;
-    // Ok(calculate_result(polymer, &pair_insertion_rules, 10))
-    for _ in 0..10 {
-        polymer.apply(&pair_insertion_rules);
-    }
-    Ok(polymer.result())
+    let (polymer, pair_insertion_rules) = parse_input(input)?;
+    Ok(calculate_result(&polymer, &pair_insertion_rules, 10))
 }
 
 fn part_2(input: &str) -> Result<usize, ParseError> {
-    let (mut polymer, pair_insertion_rules) = parse_input(input)?;
-    for _ in 0..40 {
-        polymer.apply(&pair_insertion_rules);
-    }
-    Ok(polymer.result())
+    let (polymer, pair_insertion_rules) = parse_input(input)?;
+    Ok(calculate_result(&polymer, &pair_insertion_rules, 40))
 }
 
 fn parse_input(input: &str) -> Result<(Polymer, PairInsertionRules), ParseError> {
@@ -56,7 +45,7 @@ type ElementCounts = HashMap<u8, usize>;
 type ElementCountCache = HashMap<([u8; 2], usize), ElementCounts>;
 
 fn calculate_result(
-    polymer: Polymer,
+    polymer: &Polymer,
     pair_insertion_rules: &PairInsertionRules,
     steps: usize,
 ) -> usize {
@@ -65,7 +54,7 @@ fn calculate_result(
         .0
         .windows(2)
         .map(|pair| count_elements([pair[0], pair[1]], steps, pair_insertion_rules, &mut cache))
-        .flat_map(|counts| counts.into_iter())
+        .flat_map(IntoIterator::into_iter)
         .fold(HashMap::new(), |mut counts, (element, count)| {
             *counts.entry(element).or_insert(0) += count;
             counts
@@ -73,22 +62,27 @@ fn calculate_result(
 
     // Subtract double counted elements.
     for element in polymer.0.iter().skip(1).take(polymer.0.len() - 2) {
-        *counts.get_mut(&element).unwrap() -= 1;
+        *counts.get_mut(element).unwrap() -= 1;
     }
 
     counts.values().max().unwrap() - counts.values().min().unwrap()
 }
 
-fn count_elements<'a, 'b>(
+fn count_elements(
     pair: [u8; 2],
     steps: usize,
-    pair_insertion_rules: &'a PairInsertionRules,
-    cache: &'b mut ElementCountCache,
+    pair_insertion_rules: &PairInsertionRules,
+    cache: &mut ElementCountCache,
 ) -> ElementCounts {
     if let Some(result) = cache.get(&(pair, steps)) {
         result.clone()
     } else if steps == 0 {
-        let result: ElementCounts = pair.into_iter().map(|element| (element, 1)).collect();
+        let result: ElementCounts = pair
+            .into_iter()
+            .fold(HashMap::new(), |mut result, element| {
+                *result.entry(element).or_insert(0) += 1;
+                result
+            });
         cache.insert((pair, steps), result.clone());
         result
     } else {
@@ -96,52 +90,24 @@ fn count_elements<'a, 'b>(
         let mut result = [[pair[0], insert], [insert, pair[1]]]
             .into_iter()
             .map(|pair| count_elements(pair, steps - 1, pair_insertion_rules, cache))
-            .flat_map(|counts| counts.into_iter())
+            .flat_map(IntoIterator::into_iter)
             .fold(HashMap::new(), |mut counts, (element, count)| {
                 *counts.entry(element).or_insert(0) += count;
                 counts
             });
         *result.get_mut(&insert).unwrap() -= 1;
+        cache.insert((pair, steps), result.clone());
         result
     }
 }
 
 struct Polymer(Vec<u8>);
 
-impl Polymer {
-    fn apply(&mut self, pair_insertion_rules: &PairInsertionRules) {
-        self.0 = self
-            .0
-            .windows(2)
-            .flat_map(|pair| {
-                let to_insert = pair_insertion_rules.get(pair).unwrap();
-                [pair[0], *to_insert]
-            })
-            .chain(self.0.last().into_iter().copied())
-            .collect();
-    }
-
-    fn result(&self) -> usize {
-        let counts = self.0.iter().fold(HashMap::new(), |mut counts, element| {
-            *counts.entry(element).or_insert(0) += 1;
-            counts
-        });
-
-        counts.values().max().unwrap() - counts.values().min().unwrap()
-    }
-}
-
 impl FromStr for Polymer {
     type Err = ParseError;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         Ok(Self(input.bytes().collect()))
-    }
-}
-
-impl Display for Polymer {
-    fn fmt(&self, formatter: &mut Formatter) -> FormatResult {
-        write!(formatter, "{}", std::str::from_utf8(&self.0).unwrap())
     }
 }
 
@@ -167,20 +133,17 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part_2() {
         assert_eq!(part_2(SAMPLE_INPUT).unwrap(), 2_188_189_693_529);
-        assert_eq!(part_2(INPUT).unwrap(), 0);
+        assert_eq!(part_2(INPUT).unwrap(), 2_942_885_922_173);
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_1(b: &mut Bencher) {
         b.iter(|| part_1(INPUT).unwrap());
     }
 
     #[bench]
-    #[ignore]
     fn bench_part_2(b: &mut Bencher) {
         b.iter(|| part_2(INPUT).unwrap());
     }
